18fe4469eea851c45d0e92377746aa53ee93ae21
JanisSkabs <94110515+JanisSkabs@users.noreply.github.com>
2021-12-07 10:05:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+print("Telefona grāmata")
+
+print("-----------------")
+
+kontakti = {"vards":[], "telefons":[]}
+
+kontakti["vards"] = ["Anna", "Jānis", "Miks", "Armins"]
+
+kontakti["telefons"] = ["+37134543243", "+37229768543", "+37129876500", "+37526593850"]
+
+while True:
+
+  izvelne = int(input("""Ko tu vēlies darīt? 1-izprintet 2-pievienot 3-dzēst 4-iziet\n""" ))
+
+if menu_item == 1:
+
+  print("Tava telefona grāmatiņa:")
+
+kontakti_garums = len(contacts["Name"])
+
+elif menu_item ==2:
+
+elif menu_item ==3:
+
+elif menu_item ==2:
+
+else:
+
+print("Tāda izvēle neeksistē")
